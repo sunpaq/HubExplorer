ea2caa1ae848f475fbac0400e219287b5c202809
sunyuli <sunpaq@gmail.com>
2023-01-21 17:40:01 +0900

    create xcode project

---
+++ HubExplorer/HubExplorer/HubExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubExplorerApp.swift
+//  HubExplorer
+//
+//  Created by 孙御礼 on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct HubExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
